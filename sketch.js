ff34bafb46c4980fea2a95e1d9ed689a892edafd
Alexander Kraus <akraus53@gmail.com>
2019-04-08 16:03:47 +0200

    .

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+/* 
+split the image in multiple images of the same size.
+height/width should be between 0.5 and 1
+
+*/
+
+let img;
+
+function preload() {
+  img = loadImage("mountain.jpg");
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  image(img);
+  //let b_load = createFileInput(loadRating);
+
+  splitImage(img);
 }
 
 function draw() {
   // put drawing code here
+}
+
+function loadRating(picture) {
+  console.log("Done");
+}
+
+function splitImage(image) {
+  let subimage = get(0, 0, image.width, image.width);
+  createImage(subimage);
 }
